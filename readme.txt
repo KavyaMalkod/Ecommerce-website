09eb7c6873c697c1eef0cb622de7aa513aa48f28
KavyaMalkod <kavyamalkod@gmail.com>
2022-11-27 22:36:32 +0530

    Initial commit

---
+++ readme.txt
@@ -0,0 +1,7 @@
+Company Name: Sanskriti
+Online treditional clothing Company
+
+
+Special Features:
+Slider containing three images and captions
+Contact form, Login and Sign-up form are  validated
---
+++ script.js
@@ -0,0 +1,132 @@
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+    function login()
+	{
+		var uname = document.getElementById("email").value;
+		var pwd = document.getElementById("pwd1").value;
+		var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+		if(uname =='')
+		{
+			alert("please enter user name.");
+		}
+		else if(pwd=='')
+		{
+        	alert("enter the password");
+		}
+		else if(!filter.test(uname))
+		{
+			alert("Enter valid email id.");
+		}
+		else if(pwd.length < 6 || pwd.length > 6)
+		{
+			alert("Password min and max length is 6.");
+		}
+		else
+		{
+	alert('Thank You for Login & You are Redirecting to Sanskriti e-commerce Website');
+  //Redirecting to other page or webste code or you can set your own html page.
+       window.location = "https://e-commerce-website.kavyahegde.repl.co";
+			}
+	}
+
+  function signup()
+	{
+    var name1 = document.getElementById("name").value;
+		var uname = document.getElementById("email").value;
+		var pwd = document.getElementById("pwd1").value;
+    var pw = document.getElementById("pwd2").value;
+		var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+		if(name1 =='')
+		{
+			alert("please enter name.");
+		}
+    else if(uname=='')
+		{
+        	alert("enter the email id");
+		}
+		else if(pwd=='')
+		{
+        	alert("enter the password");
+		}
+		else if(!filter.test(uname))
+		{
+			alert("Enter valid email id.");
+		}
+		else if(pwd.length < 6 || pwd.length > 6)
+		{
+			alert("Password min and max length is 6.");
+		}
+		else if(pwd==pw)
+		{
+	alert('Thank You for Sign-up & You are Redirecting to Sanskriti e-commerce Website');
+  //Redirecting to other page or webste code or you can set your own html page.
+       window.location = "https://e-commerce-website.kavyahegde.repl.co";
+			}
+    else
+		{
+			alert("confirmation of the password is wrong please re-enter");
+		}
+	}
+
+  function validate(){
+  var name = document.getElementById("name").value;
+  var phone = document.getElementById("phone").value;
+  var email = document.getElementById("email").value;
+  var message = document.getElementById("message").value;
+  var error_message = document.getElementById("error_message");
+  
+  error_message.style.padding = "10px";
+  
+  var text;
+  if(name.length < 5){
+    text = "Please Enter valid Name";
+    error_message.innerHTML = text;
+    return false;
+  }
+  if(isNaN(phone) || phone.length != 10){
+    text = "Please Enter valid Phone Number";
+    error_message.innerHTML = text;
+    return false;
+  }
+  if(email.indexOf("@") == -1 || email.length < 6){
+    text = "Please Enter valid Email";
+    error_message.innerHTML = text;
+    return false;
+  }
+  if(message.length <= 140){
+    text = "Please Enter More Than 140 Characters";
+    error_message.innerHTML = text;
+    return false;
+  }
+  alert("Form Submitted Successfully!");
+  return true;
+}
